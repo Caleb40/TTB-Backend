d3fd403ac250054f183ebcc836b7372559a56d12
Caleb <calebseyi123@gmail.com>
2023-05-05 16:45:15 +0100

    made minor changes to whitenoise settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -88,7 +88,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LANGUAGE_CODE = 'en-us'
 
