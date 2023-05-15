1978c8d91fb81a56061ee90db57f560b19de0f9b
Caleb <calebseyi123@gmail.com>
2023-05-15 17:06:07 +0100

    Changed some things in email settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,12 +25,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/ttb_media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'TopTierBinary <noreply@toptierbinary.com>'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-DEFAULT_FROM_EMAIL = 'TTB team @ toptierbinary.com'
 
 LOGGING = {
     "version": 1,
