14b90a9f59aa643d718e6137bd8ada2f79829610
Caleb <calebseyi123@gmail.com>
2023-05-16 16:36:08 +0100

    Added transaction sucessful email to signals

--- ttb_backend/signals.py
+++ ttb_backend/signals.py
@@ -1,13 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.mail import send_mail
 from .models import Transaction
 
 
 @receiver(post_save, sender=Transaction)
 def update_user_total_deposit(sender, instance, created, **kwargs):
-    if created and instance.transaction_type == 'DEPOSIT':
+    if created and instance.transaction_type == 'DEPOSIT' and instance.status == 'S':
         # Update the user's total_deposit attribute
         instance.user.total_deposit += instance.amount
+        instance.user.account_balance += instance.amount
         instance.user.save()
 
 
@@ -17,3 +19,14 @@ def add_gift_bonus(sender, instance, created, **kwargs):
         # Add gift bonus to the user's account_balance attribute
         instance.user.account_balance += instance.gift_bonus
         instance.user.save()
+
+
+@receiver(post_save, sender=Transaction)
+def send_email_on_transaction_sucessful(sender, instance, created, **kwargs):
+    if instance.status == 'S':
+        subject = 'Transaction Successful'
+        message = f'Your transaction of amount £{instance.amount} was successful.' \
+                  f' Your account balance will be updated shortly, thanks for your patronage.'
+        recipient_list = [
+            instance.user.email]
+        send_mail(subject, message, from_email=None, recipient_list=recipient_list)
